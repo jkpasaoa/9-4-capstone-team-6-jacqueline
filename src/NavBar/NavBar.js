fa3091116929271dbca2f2419328a5353b2f0e5c
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-20 15:19:28 -0400

    added Links and logo to navbar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-//import logo from './Assets/logo.PNG'
+import logo from "../assets/CityWhispererLogo.png"
 
 function NavBar() {
   return (
@@ -9,10 +9,20 @@ function NavBar() {
         {/* <img src="">
 
         </img> */}
+        <Link to='/'>
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="nav-links">
         <div>
-
+        <h3>
+          <Link to='/about'>
+        </Link>
+        </h3>
+        <h3>
+          <Link to='/createnewtour'>
+        </Link>
+        </h3>
         </div>
       </div>
     </nav>
