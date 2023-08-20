67f5f8bdf6a2d6ec8c97595d52c0b27dad7c2fa8
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-20 17:24:39 -0400

    added footer code, fixing logo route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
+//import CreateNewTour from './Components/CreateNewTour/CreateNewTour
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           {/* <Route path="/home" element={<LandingPage />} /> */}
           {/* <Route path="/about" element={<About />} /> */}
           {/* <Route path="*" element={<NotFound />} /> */}
+           {/* <Route path="/createnewtour" element={<CreateNewTour />} /> */}
         </Routes>
         </main>
         <Footer />
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom"
-import logo from "../assets/CityWhispererLogo.png"
+//import logo from "..//assets/CityWhispererLogo.png"
 
 function NavBar() {
   let loc = useLocation()
@@ -10,7 +10,7 @@ function NavBar() {
     <nav className="navbar" style={loc.pathname === "/" ? { display: "none" } : { display: "flex" }}>
       <div className="logo">
         <Link to='/'>
-          <img src={logo} alt="logo" />
+          {/* <img src={logo} alt="logo" /> */}
         </Link>
         <h1><Link to='/home'>City Whisperer</Link></h1>
       </div>
