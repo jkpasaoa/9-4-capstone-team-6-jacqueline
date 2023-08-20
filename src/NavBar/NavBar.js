f79c2b2df8e212018da46da86170d62ac16c19af
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-20 14:56:04 -0400

    foundations of navbar added

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+//import logo from './Assets/logo.PNG'
+
+function NavBar() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default NavBar;
