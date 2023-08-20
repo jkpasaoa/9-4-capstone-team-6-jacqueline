a57579d95818ac1c978a5f2ed7730fdb4952ffc7
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-20 17:07:06 -0400

    added footer basics and adjusted app.js routes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,9 @@
-export default function Footer () {
-    return (
-        <div>Footer</div>
-    )
+import './Footer.css'
+import { Link } from 'react-router-dom'
+//import logo from '../Assets
+
+function Footer() {
+
 }
+
+export default Footer;
--- src/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-import { Link } from 'react-router-dom'
-//import logo from '../Assets
-
-function Footer() {
-
-}
-
-export default Footer;
