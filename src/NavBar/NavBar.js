938fff913b7ef5d78ce885a22d72beb953253232
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-20 15:27:38 -0400

    added logo and nav links

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,28 +1,29 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom"
 import logo from "../assets/CityWhispererLogo.png"
 
 function NavBar() {
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        {/* <img src="">
+  let loc = useLocation()
 
-        </img> */}
+  return (
+    <nav className="navbar" style={loc.pathname === "/" ? { display: "none" } : { display: "flex" }}>
+      <div className="logo">
         <Link to='/'>
           <img src={logo} alt="logo" />
         </Link>
+        <h1><Link to='/home'>City Whisperer</Link></h1>
       </div>
       <div className="nav-links">
         <div>
-        <h3>
-          <Link to='/about'>
-        </Link>
-        </h3>
-        <h3>
-          <Link to='/createnewtour'>
-        </Link>
-        </h3>
+          <h3>
+            <Link to='/about'>
+            </Link>
+          </h3>
+          <h3>
+            <Link to='/createnewtour'>
+            </Link>
+          </h3>
         </div>
       </div>
     </nav>
