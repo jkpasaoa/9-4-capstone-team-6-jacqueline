83a4c6c58d0fcd7786105c6120c4cdc0e97b7518
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-21 10:19:15 -0400

    adjusted the navbar to be visible

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,29 +1,30 @@
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom"
-// import logo from "..//assets/CityWhispererLogo.png"
-
-import "./NavBar.css"
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
-  let loc = useLocation()
+  //let loc = useLocation();
 
   return (
-    <nav className="navbar" style={loc.pathname === "/" ? { display: "none" } : { display: "flex" }}>
+    <nav
+      className="navbar"
+    // style={loc.pathname === '/' ? { display: 'none' } : { display: 'flex' }}
+    >
       <div className="logo">
-        <Link to='/'>
+        <Link to="/">
           {/* <img src={logo} alt="logo" /> */}
         </Link>
-        <h1><Link to='/home'>City Whisperer</Link></h1>
+        <h1>
+          <Link to="/home">City Whisperer</Link>
+        </h1>
       </div>
       <div className="nav-links">
         <div>
           <h3>
-            <Link to='/about'>
-            </Link>
+            {/* <Link to="/about">About</Link> */}
           </h3>
           <h3>
-            <Link to='/createnewtour'>
-            </Link>
+            <Link to="/createnewtour">Create New Tour</Link>
           </h3>
         </div>
       </div>
