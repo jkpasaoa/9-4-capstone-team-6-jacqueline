d363f37695d5b52b11fe6981d0a7dab52b6f5446
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-21 20:12:31 -0400

    tested code for hamburger

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
+//import logo from "../Assets/CityWhispererLogo.png"
 
 function NavBar() {
   //let loc = useLocation();
@@ -33,3 +34,46 @@ function NavBar() {
 }
 
 export default NavBar;
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import './NavBar.css';
+// //import logo from "../Assets/CityWhispererLogo.png"
+
+// function NavBar() {
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setMenuOpen(!menuOpen);
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="hamburger-icon" onClick={toggleMenu}>
+//         <div className="line"></div>
+//         <div className="line"></div>
+//         <div className="line"></div>
+//       </div>
+//       <div className="logo">
+//         <Link to="/">
+//           <img src={logo} alt="logo" />
+//         </Link>
+//         <h1>
+//           <Link to="/home">City Whisperer</Link>
+//         </h1>
+//       </div>
+//       <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
+//         <div>
+//           <h3>
+//             {/* <Link to="/about">About</Link> */}
+//           </h3>
+//           <h3>
+//             <Link to="/createnewtour">Create New Tour</Link>
+//           </h3>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
