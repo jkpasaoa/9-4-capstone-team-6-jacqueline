f9632d39283a6aff339b4e0308ac42a7b7719ab7
arerimr <raydelys.morrobelr@gmail.com>
2023-08-22 20:33:51 -0400

    imported route components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import LandingPage from './Components/Pages/LandingPage';
+import About from './Components/Pages/About';
+import NotFound from './Components/Pages/NotFound';
+import CreateNewTour from './Components/Pages/CreateNewTour';
 
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
@@ -11,12 +16,12 @@ function App() {
       <Router>
         <NavBar />
         <main>
-        <Routes>
-          {/* <Route path="/home" element={<LandingPage />} /> */}
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-           {/* <Route path="/createnewtour" element={<CreateNewTour />} /> */}
-        </Routes>
+          <Routes>
+            <Route path="/home" element={<LandingPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/createnewtour" element={<CreateNewTour />} />
+          </Routes>
         </main>
         <Footer />
       </Router>
