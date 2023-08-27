695bb0e36d92ef828c4682fb2fef0edf906281e1
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-08-27 14:53:27 -0400

    added padding for create new tour page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import LandingPage from './components/Pages/LandingPage';
-import About from './components/Pages/About';
-import NotFound from './components/Pages/NotFound';
-import CreateNewTour from './components/Pages/CreateNewTour';
+import LandingPage from './Components/Pages/LandingPage';
+import About from './Components/Pages/About';
+import NotFound from './Components/Pages/NotFound';
+import CreateNewTour from './Components/Pages/CreateNewTour';
 
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
+import NavBar from './Components/NavBar/NavBar';
+import Footer from './Components/Footer/Footer';
 //import CreateNewTour from './components/CreateNewTour/CreateNewTour'
 //import BrowseTours from './components/BrowseTours'
 
--- src/Components/Pages/CreateNewTour.js
+++ src/Components/Pages/CreateNewTour.js
@@ -69,7 +69,7 @@ export default function CreateNewTour() {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-5" style={{ paddingTop: "160px" }}>
       <h1 className="text-center mb-4">Walking Tour Generator</h1>
       <div className="row mb-3">
         <div className="col-md-4">
