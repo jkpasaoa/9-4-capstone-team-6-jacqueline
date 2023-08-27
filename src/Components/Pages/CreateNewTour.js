fd6a88a573651f1354c2e9567b57ca96f577d14a
arerimr <raydelys.morrobelr@gmail.com>
2023-08-27 16:06:01 -0400

    added post request to send new tour info into tour sql table

--- src/Components/Pages/CreateNewTour.js
+++ src/Components/Pages/CreateNewTour.js
@@ -1,9 +1,12 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 // import MapContainer from './MapContainer';
 
 const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+
+const API = process.env.REACT_APP_API_URL;
 
 export default function CreateNewTour() {
   const [tour, setTour] = useState({
@@ -14,8 +17,9 @@ export default function CreateNewTour() {
     duration: 'Full-day',
     difficulty: 'Medium',
     tourType: 'Historic',
-    theme: '',
   });
+
+  const navigate = useNavigate()
 
   const [pointsOfInterest, setPointsOfInterest] = useState({
     latitude: 0.0,
@@ -93,6 +97,18 @@ export default function CreateNewTour() {
     }
   };
 
+  const addTour = (newTour) => {
+    axios.post(`${API}/tours`, newTour)
+      .then(() => navigate('/tours'))
+      .catch((e) => console.warn(e))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTour(tour)
+    generateWalkingTour()
+  }
+
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">Walking Tour Generator</h1>
@@ -163,7 +179,7 @@ export default function CreateNewTour() {
       </div>
       <div className="row mb-3">
         <div className="col">
-          <button className="btn btn-primary" onClick={generateWalkingTour} disabled={!tour.city || loading}>
+          <button className="btn btn-primary" onClick={handleSubmit} disabled={!tour.city || loading}>
             Generate Walking Tour
           </button>
         </div>
