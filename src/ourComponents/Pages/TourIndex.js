deffe4d9a38f003583ea9088fecfb32bc54ceebf
arerimr <raydelys.morrobelr@gmail.com>
2023-09-01 21:28:47 -0400

    index setup

--- src/ourComponents/Pages/TourIndex.js
+++ src/ourComponents/Pages/TourIndex.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import Map from "../Map/Map"
 import axios from 'axios';
 
+const API = process.env.REACT_APP_API_URL;
+
 export default function TourIndex() {
     const [tours, setTours] = useState([])
 
+    axios.get(`${API}/tours`)
+        .then((res) => setTours(res))
+        .catch((e) => console.warn(e))
+
     return (
-      <div>
-        <Map />
-  
-      </div>
+        <div>
+            <Map />
+
+        </div>
     )
 }
