a9685972b5a4cb8ffe0bb9776edee776bcccc4d4
Mark Robertson <markrobertson67@gmail.com>
2023-09-04 16:31:18 -0400

    Fixed home page and deleted landing page as smae thing.  Also fixed the create tour and it is now successfully updating in the database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import LandingPage from './ourComponents/Pages/LandingPage';
+import Home from './ourComponents/Pages/Home';
 import About from './ourComponents/Pages/About';
 import NotFound from './ourComponents/Pages/NotFound';
 import CreateNewTour from './ourComponents/Pages/CreateNewTour';
 
 import NavBar from './ourComponents/NavBar/NavBar';
 import Footer from './ourComponents/Footer/Footer';
-//import CreateNewTour from './ourComponents/CreateNewTour/CreateNewTour'
+
 //import BrowseTours from './ourComponents/BrowseTours'
 
 function App() {
@@ -20,10 +21,11 @@ function App() {
         <div className="content-container">
         {/* Check App.css for content-container for padding */}
           <Routes>
-            <Route path="/" element={<LandingPage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
             <Route path="/createnewtour" element={<CreateNewTour />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           </div>
         </main>
