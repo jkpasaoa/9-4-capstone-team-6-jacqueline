bf6d36c5bf05d48d6dcfa1e40ff75b6cd632059c
Mark Robertson <markrobertson67@gmail.com>
2023-09-04 17:24:22 -0400

    Added pointsOfInterest code

---
+++ src/ourComponents/Pages/PointsOfInterest.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+export default function PointsOfInterestList({ tourContent }) {
+  // Function to parse points of interest
+  const parsePointsOfInterest = (tourContent) => {
+    const bulletPattern = /^\s*\d+\.\s(.+)$/gm;
+    const matches = [];
+    let match;
+    while ((match = bulletPattern.exec(tourContent)) !== null) {
+      matches.push(match[1]);
+    }
+    return matches;
+  };
+
+  const pointsOfInterest = parsePointsOfInterest(tourContent);
+
+  return (
+    <div className="row">
+      <div className="col">
+        <ul>
+          {pointsOfInterest.map((poi, index) => (
+            <li key={index}>{poi}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
