d28e1b3382ebe6c21c8a039c9c4006f6a9713494
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-09-05 14:05:06 -0400

    burger menu navlinks fix to hide

--- src/ourComponents/NavBar/NavBar.js
+++ src/ourComponents/NavBar/NavBar.js
@@ -53,7 +53,7 @@ function NavBar() {
             </button>
           </div>
           <div
-            className={`items-center justify-between w-full md:w-auto md:order-1 ${menuOpen ? 'block' : ''}`}
+            className={`items-center justify-between w-full md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'}`}
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
