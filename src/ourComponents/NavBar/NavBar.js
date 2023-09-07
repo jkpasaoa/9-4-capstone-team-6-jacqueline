6baf8237b674f6bedbf73edeb4aafe09be0459f8
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-09-06 20:31:56 -0400

    changed hamburger color

--- src/ourComponents/NavBar/NavBar.js
+++ src/ourComponents/NavBar/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
 
   return (
     <div>
-      <nav className="bg-white-200 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 custom-border">
+      <nav className="bg-white-200 fixed w-full z-20 top-0 left-0 border-b-0 border-gray-200 dark:border-gray-600 custom-border">
         {/* opacity-50 */}
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <span className="logo">
@@ -49,6 +49,7 @@ function NavBar() {
               aria-expanded={menuOpen}
             >
               <span className="sr-only">Open main menu</span>
+              <div className="bg-red-500 rounded-md p-2">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -64,6 +65,7 @@ function NavBar() {
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
+              </div>
             </button>
           </div>
           {/* Mobile Menu */}
@@ -110,129 +112,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import './NavBar.css';
-// import logo from '../../assets/CityWhispererLogoWebsite.png';
-
-// function NavBar() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <div>
-//       <nav className="bg-white-200 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-//         {/* opacity-50 */}
-//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-//           <span className="logo">
-//             <Link to="/home" className="flex items-center">
-//               <img src={logo} className="h-24 mr-3 drop-shadow-[2px_0px_5px_rgba(255,255,255,0.5)
-// ]" alt="CityWhisperer Logo" />
-//             </Link>
-//           </span>
-//               {/* Desktop Menu & Tablet Menu */}
-//               <ul className="DESKTOP-MENU hidden space-x-8 md:flex lg:flex">
-//             <li>
-//               <Link to="/home" className="nav-link home text-black text-shadow-white">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about" className="nav-link about text-black text-shadow-white">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/browsetours" className="nav-link browsetours text-black text-shadow-white">Browse Tours</Link>
-//             </li>
-//           </ul>
-//         </div>
-//           <div className="flex items-center ml-4">
-//             <Link
-//               to="/createnewtour"
-//               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-//             >
-//               <strong>Create New Tour</strong>
-//             </Link>
-//             <button
-//               onClick={toggleMenu}
-//               data-collapse-toggle="navbar-sticky"
-//               type="button"
-//               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-//               aria-controls="navbar-sticky"
-//               aria-expanded={menuOpen}
-//             >
-//               <span className="sr-only">Open main menu</span>
-//               <svg
-//                 className="w-5 h-5"
-//                 aria-hidden="true"
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 fill="none"
-//                 viewBox="0 0 17 14"
-//               >
-//                 <path
-//                   stroke="currentColor"
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M1 1h15M1 7h15M1 13h15"
-//                 />
-//               </svg>
-//             </button>
-//           </div>
-//           {/* Mobile Menu */}
-//           <div
-//             className={`items-center justify-between w-full md:w-auto md:order-1 ${menuOpen ? 'block' : 'hidden'
-//               } md:hidden`} // Show on mobile screens
-//             id="navbar-sticky"
-//           >
-//             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-//               <li>
-//                 <Link
-//                   to="/home"
-//                   onClick={toggleMenu}
-//                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-//                   aria-current="page"
-//                 >
-//                   Home
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link
-//                   to="/about"
-//                   onClick={toggleMenu}
-//                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-//                 >
-//                   About
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link
-//                   to="/browsetours"
-//                   onClick={toggleMenu}
-//                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-//                 >
-//                   Browse Tours
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//           {/* Desktop Menu & Tablet Menu */}
-//           {/* <ul className="DESKTOP-MENU hidden space-x-8 md:flex lg:flex">
-//             <li>
-//               <Link to="/home" className="nav-link home text-black text-shadow-white">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about" className="nav-link about text-black text-shadow-white">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/browsetours" className="nav-link browsetours text-black text-shadow-white">Browse Tours</Link>
-//             </li>
-//           </ul>
-//         </div> */}
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default NavBar;
