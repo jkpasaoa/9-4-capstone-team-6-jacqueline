82996ebe6069a5b85ab62ba0b7281f8c37159741
arerimr <raydelys.morrobelr@gmail.com>
2023-09-07 21:15:35 -0400

    finished functionality still needs color palette and real pics from backend

--- src/ourComponents/Pages/TourCard.js
+++ src/ourComponents/Pages/TourCard.js
@@ -4,12 +4,7 @@ export default function TourCard({ tour }) {
     return (
         <div>
             <h3>{tour.region ? tour.region : tour.city} {tour.duration} {tour.theme} Tour</h3>
-            <h4>{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country}</h4>
             <span>Difficulty: {tour.difficulty}</span>
-            <hr></hr>
-            <br>
-            </br>
-            <p></p>
         </div>
     )
 }
