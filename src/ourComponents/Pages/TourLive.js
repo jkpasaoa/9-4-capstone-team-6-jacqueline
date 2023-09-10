45b9d911370e1576083d8f5088197105ac330a77
arerimr <raydelys.morrobelr@gmail.com>
2023-09-10 13:56:25 -0400

    added route to tourDetails/tourLive page

--- src/ourComponents/Pages/TourLive.js
+++ src/ourComponents/Pages/TourLive.js
@@ -1,10 +1,13 @@
-
-import './Pages.css'; 
+import { useParams } from 'react-router-dom';
+import './Pages.css';
 
 export default function Tour() {
+
+    const { id } = useParams()
+    
     return (
         <div>
-<p className="TourLive-content">Welcome to your tour</p>
+            <p className="TourLive-content">Welcome to your tour</p>
         </div>
     )
 }
