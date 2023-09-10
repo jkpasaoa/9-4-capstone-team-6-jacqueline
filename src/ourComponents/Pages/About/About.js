11bd8aa9032375637f5fc11aa1f5bc696023b4d6
Jacqueline Pasaoa <jacqueline.pasaoa@gmail.com>
2023-09-10 15:55:05 -0400

    photos of team members adjusted and added onto about

--- src/ourComponents/Pages/About/About.js
+++ src/ourComponents/Pages/About/About.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../About/About.css'
 // import TeamMemberCard from "./TeamMemberCard";
-// import JosephPhoto from "..//assets/Joseph.jpg"
-// import RaydelysPhoto from "..//assets/Raydelys.jpg"
-// import jacquelinePhoto from "..//assets/jacqueline.jpg"
-// import MarkPhoto from "..//assets/Mark.jpg"
+import JosephPhoto from "../../../assets/Joseph.jpg"
+import RaydelysPhoto from "../../../assets/Raydelys.jpg"
+import JacquelinePhoto from "../../../assets/Jacqueline.jpg"
+import MarkPhoto from "../../../assets/Mark.jpg"
 
 function About() {
 
   const teamMembers = [
     {
       name: "Joseph Rodriquez",
-      // photo: JosephPhoto,
+      photo: JosephPhoto,
       bio: `I am Joseph Rodriguez, a compassionate and driven junior full stack developer. With a passion for technology and empathy for users, I excel in creating innovative solutions. Continuously seeking growth and learning, I aim to make a positive impact in the ever-changing world of software development.`,
       github: "https://github.com/jRodriguezIV",
       linkedin: "https://www.linkedin.com/in/josephrodrigueziv/",
@@ -19,7 +20,7 @@ function About() {
 
     {
       name: "Mark Roberston",
-      // photo: MarkPhoto,
+      photo: MarkPhoto,
       bio: `I'm currently a Full Stack Web Development Fellow at Pursuit, a 12-month, Google-funded software engineering fellowship with a 9% acceptance rate. I also enjoy movies, Formula 1, travel, and playing with my German Shepherd.
 
           With financial markets experience and would enjoy working for a financial firm.`,
@@ -30,7 +31,7 @@ function About() {
 
     {
       name: "Raydelys M. Reyes",
-      // photo: RaydelysPhoto,
+      photo: RaydelysPhoto,
       bio: `Presently, I hold the title of a Full Stack Web Development Fellow at Pursuit, a prestigious 12-month software engineering fellowship with funding from Google. With an acceptance rate of just 9%.
 
          My ultimate goal is to become a skilled software engineer capable of tackling real-world challenges.`,
@@ -40,7 +41,7 @@ function About() {
 
     {
       name: "Jacqueline Pasaoa",
-      // photo: jacquelinePhoto,
+      photo: JacquelinePhoto,
       bio: `Jacqueline is full stack developer that studied under Pursuit. She underwent comprehensive training to master both front-end and back-end technologies. She loves to travel, eat ice cream, and wants to learn more coding languages. And hopes to add more cities to travel to in the near future.`,
       github: "https://github.com/jkpasaoa",
       linkedin: "https://www.linkedin.com/in/jacquelinepasaoa/",
@@ -67,16 +68,26 @@ function About() {
         <div className="team">
           <h1>Meet the Team</h1>
           <div className="team-members">
-            {teamMembers.map((member, index) => (
-              <div key={index} className="team-member-card">
-                <h2 className="text-lg font-semibold">{member.name}</h2>
-                <p>{member.bio}</p>
-                <div className="social-links">
-                  <a href={member.github}>GitHub</a>
-                  <a href={member.linkedin}>LinkedIn</a>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              {teamMembers.map((member, index) => (
+                <div key={index} className="team-member-card">
+                  <img src={member.photo}
+                    alt="memberphoto"
+                    style={{
+                      width: '150px',
+                      height: '170px',
+                      borderRadius: '0px',
+                      objectFit: 'cover',
+                    }} />
+                  <h2 className="text-lg font-semibold">{member.name}</h2>
+                  <p>{member.bio}</p>
+                  <div className="social-links">
+                    <Link to={member.github}>GitHub</Link>
+                    <Link to={member.linkedin}>LinkedIn</Link>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
