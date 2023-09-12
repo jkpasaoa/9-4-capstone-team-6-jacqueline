4b7f6af7e0d137c84af76975c3259a5810af1052
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-09-12 18:53:27 -0400

    got carousel to finally show, however the video is not actually playing yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/tw-elements/dist/js/**/*.js"
+    ],
+    theme: {
+      extend: {},
+    },
+    darkMode: "class",
+    plugins: [require("tw-elements/dist/plugin.cjs")]
+  }
