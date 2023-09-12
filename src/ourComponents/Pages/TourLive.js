ca6ee3df387de8ebc73fcbd43afa52839d04b7fa
arerimr <raydelys.morrobelr@gmail.com>
2023-09-12 19:53:52 -0400

    styled title and image of show page

--- src/ourComponents/Pages/TourLive.js
+++ src/ourComponents/Pages/TourLive.js
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import './Pages.css';
 import { useEffect, useState } from 'react';
 import PointOfInterestCard from './PointOfInterestCard.js';
-import './Pages.css'; 
+import './Pages.css';
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -37,12 +37,18 @@ export default function Tour() {
 
     return (
         <div className="TourLive-content">
-            <h1 className='text-3xl font-extrabold'>Welcome to your {tour.tour_name}</h1>
+            <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-cyan-600 from-sky-950'>{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country}</h1>
+            <h4 class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Welcome to your {tour.tour_name}</h4>
             {/* <p>Created on { }</p> */}
-            <img src={tour.image_url} alt={tour.city} />
+            <br />
+            <figure class="max-w-lg">
+                <img src={tour.image_url} alt={tour.city} class="h-auto max-w-full rounded-lg" />
+                <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country} Google Images©</figcaption>
+            </figure>
             <div>
                 <Map />
             </div>
+            <br></br>
             <ul>
                 {
                     pointsOfInterest.map((poi, index) => {
