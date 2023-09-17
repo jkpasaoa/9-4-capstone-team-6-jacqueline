cdc9bb44bd3f87d8f8895e3425331b8cb64abc5a
arerimr <raydelys.morrobelr@gmail.com>
2023-09-17 17:01:13 -0400

    setting up for routing

--- src/ourComponents/Map/Map.js
+++ src/ourComponents/Map/Map.js
@@ -1,6 +1,6 @@
 
 import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api'
-import { useMemo, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import loadingLogo from '../../assets/S-Loop_transnparent.gif'
 
 // const center = { lat: 40.8448, lng: 40.8448 }
@@ -8,11 +8,22 @@ import loadingLogo from '../../assets/S-Loop_transnparent.gif'
 export default function Map() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    libraries: ['places']
   })
 
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDurations] = useState('')
   const [map, setMap] = useState(/** @type google.maps.map */(null))
 
   const center = useMemo(() => ({ lat: 40.8448, lng: -73.8648 }), []);
+
+  // start and end point to be replaced with poi cordinates
+  // const originRef = useRef()
+  // const destinationRef = useRef()
+
+  const startPoint = { center }
+  const endPoint = { lat: 40.6782, lng: -73.9442 }
 
   if (!isLoaded) {
     return (
