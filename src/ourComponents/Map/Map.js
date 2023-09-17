8ae2500dddef0d7486a33f2eecd5ee72d1c21078
arerimr <raydelys.morrobelr@gmail.com>
2023-09-17 11:47:07 -0400

    added loading screen to map

--- src/ourComponents/Map/Map.js
+++ src/ourComponents/Map/Map.js
@@ -1,9 +1,18 @@
 import { useJsApiLoader } from '@react-google-maps/api'
+import loadingLogo from '../../assets/S-Loop_transnparent.gif'
 
 export default function Map() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   })
+
+  if (!isLoaded) {
+    return (
+      <div>
+        <img src={loadingLogo} alt='loading...' />
+      </div>
+    )
+  }
 
   return (
     <div>
