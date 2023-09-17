3a131265dd922cf37c66a09a54ef4f1a74e03ff7
arerimr <raydelys.morrobelr@gmail.com>
2023-09-17 16:00:37 -0400

    added 'map' to show page

--- src/ourComponents/Map/Map.js
+++ src/ourComponents/Map/Map.js
@@ -1,10 +1,15 @@
-import { useJsApiLoader } from '@react-google-maps/api'
+
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { useMemo } from 'react';
 import loadingLogo from '../../assets/S-Loop_transnparent.gif'
+
+const center = { lat: 40.7128, lng: 74.0060 }
 
 export default function Map() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   })
+  // const center = useMemo(() => ({ lat: 40.712776, lng: -74.005974 }), []);
 
   if (!isLoaded) {
     return (
@@ -15,8 +20,13 @@ export default function Map() {
   }
 
   return (
-    <div>
-      <h1>GOOGLE MAPS API HERE</h1>
+    <div position='center' className='h-[300px] w-[600px]'>
+      <GoogleMap
+        center={center}
+        zoom={15}
+        mapContainerStyle={{ width: '100%', height: '100%' }}
+      >
+      </GoogleMap>
     </div>
   )
 }
--- src/ourComponents/Pages/TourLive.js
+++ src/ourComponents/Pages/TourLive.js
@@ -37,23 +37,24 @@ export default function Tour() {
 
     return (
         <div className='tourLive'>
-            <div class="relative h-10 w-[200px] ...">
-                <button class="absolute left-0 top-0 h-16 w-[200px] font-extrabold text-sky-950 ..."><Link to='/tours'>🔙 ALL TOURS</Link></button>
+            <div className="relative h-10 w-[200px] ...">
+                <button className="absolute left-0 top-0 h-16 w-[200px] font-extrabold text-sky-950 ..."><Link to='/tours'>🔙 ALL TOURS</Link></button>
             </div>
             <div className="TourLive-content">
                 <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-cyan-600 from-sky-950'>{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country}</h1>
-                <h4 class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Welcome to your {tour.tour_name}</h4>
+                <h4 className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Welcome to your {tour.tour_name}</h4>
                 {/* <p>Created on { }</p> */}
                 <br />
-                <figure class="max-w-lg">
-                    <img src={tour.image_url} alt={tour.city} class="h-auto max-w-full rounded-lg" />
-                    <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country} Google Images©</figcaption>
+                <figure className="max-w-lg">
+                    {/* <img src={tour.image_url} alt={tour.city} className="h-auto max-w-full rounded-lg" /> */}
+                    <Map className="h-auto max-w-full rounded-lg" />
+                    <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country} Google Images©</figcaption>
                 </figure>
                 <div>
-                    <Map />
+                    {/* img here maybe */}
                 </div>
                 <br></br>
-                <h2 class="text-4xl font-bold dark:text-white text-sky-950">Points of Interest:</h2>
+                <h2 className="text-4xl font-bold dark:text-white text-sky-950">Points of Interest:</h2>
                 <ul>
                     {
                         pointsOfInterest.map((poi, index) => {
