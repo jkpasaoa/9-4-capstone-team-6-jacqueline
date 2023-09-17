daa319ce7c238f0800044c5821b683eaef116ac0
arerimr <raydelys.morrobelr@gmail.com>
2023-09-17 11:42:13 -0400

    set up google maps api

--- src/ourComponents/Map/Map.js
+++ src/ourComponents/Map/Map.js
@@ -1,6 +1,10 @@
-
+import { useJsApiLoader } from '@react-google-maps/api'
 
 export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  })
+
   return (
     <div>
       <h1>GOOGLE MAPS API HERE</h1>
