4ec48837769d9a828db18765e2fd07502a40a3d5
Jacqueline Pasaoa <jacqueline.pasaoa@gmail.com>
2023-09-17 09:50:12 -0400

    end of tour page created

---
+++ src/ourComponents/Pages/EndTour.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+
+function EndTour() {
+
+  return (
+  <div>
+    {/* test */}
+  </div>
+  )
+}
+
+export default EndTour;
