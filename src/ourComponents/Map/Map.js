f6ecde488c6e343bd9944ed8c7c1d83a8d4414b3
arerimr <raydelys.morrobelr@gmail.com>
2023-09-18 20:58:30 -0400

    fixed bug to be able to use google object

--- src/ourComponents/Map/Map.js
+++ src/ourComponents/Map/Map.js
@@ -1,6 +1,6 @@
 // /*global google*/
 import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api'
-import { useMemo, useState } from 'react'; //useRef, useEffect
+import { useMemo, useState, useEffect } from 'react'; //useRef, useEffect
 import loadingLogo from '../../assets/S-Loop_transnparent.gif'
 
 // const center = { lat: 40.8448, lng: 40.8448 }
@@ -11,39 +11,43 @@ export default function Map() {
     libraries: ['places']
   })
 
-  // const [directionsResponse, setDirectionsResponse] = useState(null)
-  // const [distance, setDistance] = useState('')
-  // const [duration, setDuration] = useState('')
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
   const [map, setMap] = useState(/** @type google.maps.Map */(null))
 
   const center = useMemo(() => ({ lat: 40.8448, lng: -73.8648 }), []);
 
-  // start and end point to be replaced with poi cordinates
+  // // start and end point to be replaced with poi cordinates
   // const originRef = useRef()
   // const destinationRef = useRef()
 
-  // const startPoint = { center }
-  // const endPoint = { lat: 40.6782, lng: -73.9442 }
+  const startPoint = { center }
+  const endPoint = { lat: 40.6782, lng: -73.9442 }
 
-  // const calculateRoute = async () => {
-  //   if (!startPoint || !endPoint) {
-  //     return
-  //   }
+  const calculateRoute = async () => {
+    if (!startPoint || !endPoint) {
+      return
+    }
 
-  //   const directionsService = new google.maps.DirectionsService()
-  //   const results = await directionsService.route({
-  //     origin: startPoint,
-  //     destination: endPoint,
-  //     travelMode: google.maps.TravelMode.WALKING
-  //   })
-  //   setDirectionsResponse(results)
-  //   setDistance(results.routes[0].legs[0].distance.text)
-  //   setDuration(results.routes[0].legs[0].duration.text)
-  // }
+    // const google = window.google;
 
-  // useEffect(() => {
-  //   calculateRoute()
-  // }, [])
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: startPoint,
+      destination: endPoint,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.WALKING
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  useEffect(() => {
+    calculateRoute()
+  }, [])
 
   if (!isLoaded) {
     return (
