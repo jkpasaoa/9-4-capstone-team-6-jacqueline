ef983fa4b7b8282e799eb5335a47bf8819f14061
Mark Robertson <markrobertson67@gmail.com>
2023-09-28 13:39:31 -0400

    Added points of interest list without coordinates on create page

--- src/ourComponents/Pages/CreateNewTour/CreateNewTour.js
+++ src/ourComponents/Pages/CreateNewTour/CreateNewTour.js
@@ -216,6 +216,7 @@ export default function CreateNewTour() {
   const [tourContent, setTourContent] = useState('');
   const [isLoading, setIsLoading] = useState(false); // Added isLoading state
   const [cityPhoto, setCityPhoto] = useState('');
+  const [poiNames, setPoiNames] = useState([]); // Create state for POI names
 
 
 
@@ -350,7 +351,8 @@ export default function CreateNewTour() {
 
       console.log('Points of Interest: ', sanitizedPointsOfInterest);
       console.log('Coordinates: ', coordinates);
-
+// Set the POI names in state
+setPoiNames(sanitizedPointsOfInterest);
       
 
       setIsLoading(false);
@@ -398,6 +400,8 @@ export default function CreateNewTour() {
 
     // Parse and sanitize points of interest with coordinates
     const { sanitizedPointsOfInterest, coordinates } = generatedWalkingTour;
+
+
 
     // Fetch the city photo
     const cityPhoto = await fetchCityPhoto(tour.city, setCityPhoto);
@@ -604,7 +608,11 @@ export default function CreateNewTour() {
 
             <br />
 
-            <textarea className="route-container border rounded-lg w-full p-2" rows="10" value={tourContent} readOnly />
+            <ol className="ordered-list">
+              {poiNames.map((poiName, index) => (
+                <li key={index}>{poiName}</li>
+              ))}
+            </ol>
           </div>
         )}
 
