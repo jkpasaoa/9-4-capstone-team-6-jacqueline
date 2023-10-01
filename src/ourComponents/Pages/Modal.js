1cbb009626fa802305af756a7bcbfb8f1bb57460
arerimr <raydelys.morrobelr@gmail.com>
2023-10-01 10:37:24 -0400

    set up modal content

---
+++ src/ourComponents/Pages/Modal.js
@@ -0,0 +1,15 @@
+
+
+export default function Modal() {
+    return (
+        <div>
+            <h1>modal content</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+                perferendis suscipit officia recusandae, eveniet quaerat assumenda
+                id fugit, dignissimos maxime non natus placeat illo iusto!
+                Sapiente dolorum id maiores dolores? Illum pariatur possimus
+                quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+                placeat tempora vitae enim incidunt porro fuga ea.</p>
+        </div>
+    )
+}
