71c03afb1baffcba87ae53a86eb6a5703a729fb3
arerimr <raydelys.morrobelr@gmail.com>
2023-10-01 12:57:39 -0400

    commentary showing on modal

--- src/ourComponents/Pages/Modal.js
+++ src/ourComponents/Pages/Modal.js
@@ -1,16 +1,13 @@
 import { AiOutlineCloseSquare } from 'react-icons/ai'
 
-export default function Modal({ toggleModal, img, name }) {
+export default function Modal({ toggleModal, img, name, commentary }) {
     return (
         <div className="modal-content content-center">
             <h1 className="text-4xl font-bold dark:text-white text-sky-950">{name}</h1>
             <img src={img} alt='img' className='w-[400px] ml-[13%]' />
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-                perferendis suscipit officia recusandae, eveniet quaerat assumenda
-                id fugit, dignissimos maxime non natus placeat illo iusto!
-                Sapiente dolorum id maiores dolores? Illum pariatur possimus
-                quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-                placeat tempora vitae enim incidunt porro fuga ea.</p>
+            <div className='max-h-[170px] overflow-y-auto'>
+                <p>{commentary}</p>
+            </div>
             <button onClick={() => toggleModal()} className="close-modal">{<AiOutlineCloseSquare />}</button>
         </div>
     )
