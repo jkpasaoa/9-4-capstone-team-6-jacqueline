3caea1e2d7d9038c7ec349329d0c550880ba9a46
arerimr <raydelys.morrobelr@gmail.com>
2023-10-01 15:01:10 -0400

    lil image styling

--- src/ourComponents/Pages/Modal.js
+++ src/ourComponents/Pages/Modal.js
@@ -6,7 +6,7 @@ export default function Modal({ toggleModal, img, name, commentary }) {
     return (
         <div className="modal-content content-center">
             <h1 className="text-4xl font-bold dark:text-white text-sky-950">{name}</h1>
-            <img src={img} alt='img' className='w-[400px] ml-[13%]' />
+            <img src={img} alt='img' className='w-[400px] ml-[13%] rounded-lg' />
             <p className="ml-3"><Link className="inline-flex text-sky-800"><HiPlay className="mt-1" /> PLAY AUDIO</Link></p>
             <div className='max-h-[170px] overflow-y-auto'>
                 <p>{commentary}</p>
