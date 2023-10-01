9aeb97704ffa6d7482c0e97213980e6c36e9c9e2
arerimr <raydelys.morrobelr@gmail.com>
2023-10-01 09:40:19 -0400

    removed img from poi cards

--- src/ourComponents/Pages/PointOfInterestCard.js
+++ src/ourComponents/Pages/PointOfInterestCard.js
@@ -39,7 +39,7 @@ export default function PointOfInterestCard({ poi_id, name, img, setActiveMarker
     return (
         <div>
 
-            <li className="text-left grid grid-cols-2 gap-7" onMouseOver={() => setActiveMarker(name)} onMouseLeave={() => setActiveMarker('')}><span className="float-left px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-[400px]"><h3 className="inline-flex text-xl font-bold"><ImLocation />{name}</h3>
+            <li className="text-left" onMouseOver={() => setActiveMarker(name)} onMouseLeave={() => setActiveMarker('')}><span className="float-left px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-[400px]"><h3 className="inline-flex text-xl font-bold"><ImLocation />{name}</h3>
                 <section className="border-l-2 border-sky-950 ml-2">
                     <p className="ml-3"><Link onClick={() => textToSpeech()} className="inline-flex text-sky-800"><HiPlay className="mt-1" /> PLAY AUDIO</Link></p>
                     <p className="ml-3">In this paragraph there will be a short description leading to full details...</p>
@@ -47,7 +47,7 @@ export default function PointOfInterestCard({ poi_id, name, img, setActiveMarker
                 </section>
 
             </span>
-                <img className="w-[200px] h-[110px]" src={img} alt={name} />
+                {/* <img className="w-[200px] h-[110px]" src={img} alt={name} /> */}
             </li>
         </div>
     )
