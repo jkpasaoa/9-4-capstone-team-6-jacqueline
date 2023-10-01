f376430d6626d92fadd7dd91d4e547e40535a4b4
Jacqueline Pasaoa <jacqueline.pasaoa@gmail.com>
2023-10-01 17:59:53 -0400

    fields container positioning left in create new tour page

--- src/ourComponents/Pages/CreateNewTour/CreateNewTour.js
+++ src/ourComponents/Pages/CreateNewTour/CreateNewTour.js
@@ -463,7 +463,9 @@ export default function CreateNewTour() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen full-background-color" style={{ paddingTop: '200px' }}>
+    <div className="flex flex-col items-center justify-center min-h-screen full-background-color"
+    style={{ paddingTop: '200px' }}
+    >
       <div className="container flex flex-col items-center justify-center ">
         <h1 className="luxury-font text-3xl text-center mb-4 font-extrabold">
           Ready to Explore?
