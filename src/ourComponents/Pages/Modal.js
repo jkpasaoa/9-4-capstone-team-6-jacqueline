9a69a12d1d201980997b9fb48dd05ba1a05ecb62
arerimr <raydelys.morrobelr@gmail.com>
2023-10-01 15:31:55 -0400

    removed warnings

--- src/ourComponents/Pages/Modal.js
+++ src/ourComponents/Pages/Modal.js
@@ -1,6 +1,7 @@
 import { AiOutlineCloseSquare } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { HiPlay } from 'react-icons/hi2'
+import { HiMiniPause } from 'react-icons/hi2'
 
 let speech = new SpeechSynthesisUtterance();
 let synth = window.speechSynthesis;
@@ -27,7 +28,7 @@ export default function Modal({ toggleModal, img, name, commentary }) {
         <div className="modal-content content-center">
             <h1 className="text-4xl font-bold dark:text-white text-sky-950">{name}</h1>
             <img src={img} alt='img' className='w-[400px] ml-[13%] rounded-lg' />
-            <p className="ml-3"><Link onClick={() => textToSpeech()} className="inline-flex text-sky-800"><HiPlay className="mt-1" /> PLAY AUDIO</Link></p>
+            <p className="ml-3 inline-flex text-sky-800"><Link onClick={() => textToSpeech()} className="inline-flex text-sky-800"><HiPlay className="mt-1" /> PLAY AUDIO </Link> <Link className="inline-flex text-sky-800" onClick={() => speechPause()}> <HiMiniPause className="mt-1" />PAUSE</Link></p>
             <div className='max-h-[170px] overflow-y-auto'>
                 <p>{commentary}</p>
             </div>
