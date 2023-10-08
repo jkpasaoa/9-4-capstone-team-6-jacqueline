e16297232f9301f865f6eec95152914a6539f7db
arerimr <raydelys.morrobelr@gmail.com>
2023-10-08 12:22:45 -0400

    capitalized state

--- src/ourComponents/Pages/TourLive.js
+++ src/ourComponents/Pages/TourLive.js
@@ -44,6 +44,14 @@ export default function Tour() {
             .catch((e) => console.warn(e))
     }, [id])
 
+    // const capitalizeFirstLetter = (word) => {
+    //     const capitalized =
+    //         word.charAt(0).toUpperCase()
+    //         + word.slice(1)
+
+    //     return capitalized
+    // }
+
     // console.log(allPointsOfInterest)
 
     // const stringToDate = (string) => {
@@ -92,7 +100,7 @@ export default function Tour() {
                     <button className="absolute inline-flex ml-[80px] mt-[40px] left-0 top-0 h-16 w-[200px] font-extrabold text-sky-950 ..."><Link className="inline-flex" to='/tours'>{<TiArrowBack />} ALL TOURS</Link></button>
                 </div>
                 <div className="TourLive-content h-screen pt-4 typewriter">
-                    <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-cyan-600 from-sky-950'>{tour.city}, {tour.state ? `${tour.state},` : null} {tour.country}</h1>
+                    <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-cyan-600 from-sky-950'>{tour.city}, {tour.state ? `${tour.state.toUpperCase()},` : null} {tour.country}</h1>
                     <div>
                         <h4 className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Welcome to your {tour.tour_name}</h4>
                     </div>
