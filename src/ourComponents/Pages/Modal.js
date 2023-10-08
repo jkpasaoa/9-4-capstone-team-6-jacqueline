5fc4bac5d811011264312b2a1a651f36b0c081d7
arerimr <raydelys.morrobelr@gmail.com>
2023-10-08 10:51:31 -0400

    added typewriter effect to commentary

--- src/ourComponents/Pages/Modal.js
+++ src/ourComponents/Pages/Modal.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { HiPlay } from 'react-icons/hi2'
 import { HiMiniPause } from 'react-icons/hi2'
 import { HiStop } from 'react-icons/hi2'
+import './Pages.css';
 
 let speech = new SpeechSynthesisUtterance();
 let synth = window.speechSynthesis;
@@ -37,8 +38,8 @@ export default function Modal({ toggleModal, img, name, commentary }) {
                 <img src={img} alt='img' className='w-[500px] rounded-lg h-[290px] mb-2' />
             </div>
             <p className="ml-3 inline-flex text-sky-800"><Link onClick={() => textToSpeech()} className="inline-flex text-sky-800"><HiPlay className="mt-1" /> PLAY </Link> &nbsp; &nbsp; &nbsp; <Link className="inline-flex text-sky-800" onClick={() => speechPause()}> <HiMiniPause className="mt-1" />PAUSE</Link> &nbsp; &nbsp; &nbsp; <Link onClick={() => speechStop()} className="inline-flex text-sky-800"><HiStop className="mt-1" /> STOP</Link>&nbsp;</p>
-            <div className='max-h-[170px] overflow-y-auto mt-[5px] mb-[10px]'>
-                <p className='text-gray-500 dark:text-gray-400 h-[150px] typewriter'>{commentary}</p>
+            <div className='max-h-[170px] overflow-y-auto mt-[5px] mb-[10px] typewriter'>
+                <p className='text-gray-500 dark:text-gray-400 h-[150px]'>{commentary}</p>
             </div>
             <button onClick={() => toggleModal()} className="close-modal">{<AiOutlineCloseSquare />}</button>
         </div>
